81d09a2994135aa2649e4d77e2c675aea2adf435
Shohabbos Usmonov <shohabbos.usmonov@kpi.com>
2022-10-30 04:35:02 +0500

    final result 2

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -6,6 +6,6 @@ public class BasicVariablesInitialization {
         int second = 10;
         int third = 100;
 
-        System.out.println(first + "\n" + second + "\n" + third);
+        System.out.println(first + "\n" + second + "\n" + third + "\n");
     }
 }
